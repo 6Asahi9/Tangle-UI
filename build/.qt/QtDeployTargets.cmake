d79f48e3e3f2faae3968c578a81e924129648e4a
Asahi <161689859+6Asahi9@users.noreply.github.com>
2026-02-11 21:26:44 +0530

    build gone

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_TangleML-UI_FILE C:/Users/Asahi/Documents/PersonalProjects/Tangle-UI/build/TangleML-UI.exe)
-set(__QT_DEPLOY_TARGET_TangleML-UI_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_TangleML-UI_RUNTIME_DLLS C:/Qt/6.10.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.10.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.10.1/mingw_64/bin/Qt6Core.dll)
